f8892d68bc26c8c12ad943510a88bd54e4599ab4
poornachandr <31505561+poornachandr@users.noreply.github.com>
2017-08-31 19:24:22 +0530

    Create num positive or negative.c

---
+++ num positive or negative.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int num;
+ printf(/n"ETNER THE NUMBER");
+ scanf("%d",&num);
+ if(num<0)
+ {
+  printf("the num is positive");
+  }
+  else
+  {
+   printf("the num is negative");
+   }
+   };
+ 
+ 
+ 
